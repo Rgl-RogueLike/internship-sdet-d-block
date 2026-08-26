d2c9121dd6bc8a5f77f79c2a8d4f2507ffb72485
Nikita Haritonov <nikita.harit@mail.ru>
2026-08-26 18:02:10 +0300

    feat(config): add Configuration interface using Owner library

--- src/test/java/com/haritonov/apitests/config/Configuration.java
+++ src/test/java/com/haritonov/apitests/config/Configuration.java
@@ -1,4 +1,28 @@
 package com.haritonov.apitests.config;
 
-public interface Configuration {
+import org.aeonbits.owner.Config;
+
+@Config.Sources("classpath:configurations/config.properties")
+public interface Configuration extends Config {
+
+    @Key("base.uri")
+    String baseUri();
+
+    @Key("base.path")
+    String basePath();
+
+    @Key("wp.username")
+    String wpUsername();
+
+    @Key("wp.password")
+    String wpPassword();
+
+    @Key("db.url")
+    String dbUrl();
+
+    @Key("db.user")
+    String dbUser();
+
+    @Key("db.password")
+    String dbPassword();
 }
