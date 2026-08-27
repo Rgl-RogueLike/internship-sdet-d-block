2baf3123b670d5cfc70337d998213ac138edae47
Nikita Haritonov <nikita.harit@mail.ru>
2026-08-27 11:02:39 +0300

    feat(api): add ApiEndpoints class for storing api paths

---
+++ src/test/java/com/haritonov/apitests/endpoints/ApiEndpoints.java
@@ -0,0 +1,12 @@
+package com.haritonov.apitests.endpoints;
+
+public final class ApiEndpoints {
+
+    private ApiEndpoints() {
+
+    }
+
+    public static final String POSTS = "/posts";
+    public static final String POST_BY_ID = "/posts/{id}";
+    public static final String USERS_ME = "/user/me";
+}
