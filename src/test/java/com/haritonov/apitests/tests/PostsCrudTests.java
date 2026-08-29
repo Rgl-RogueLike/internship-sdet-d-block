0756461bc88a5f4af8ead4efa8486fdedb33bb17
Nikita Haritonov <nikita.harit@mail.ru>
2026-08-29 09:29:51 +0300

    refactor(tests): rename test methods according to ShouldWhen convention

--- src/test/java/com/haritonov/apitests/tests/PostsCrudTests.java
+++ src/test/java/com/haritonov/apitests/tests/PostsCrudTests.java
@@ -10,7 +10,7 @@ import org.testng.annotations.Test;
 public class PostsCrudTests extends BaseTest {
 
     @Test
-    public void testCreatePostWithValidData() {
+    public void shouldCreatePostWhenValidDataProvided() {
         PostRequest request = PostRequest.builder()
                 .title(DataGenerator.generatePostTitle())
                 .content(DataGenerator.generatePostContent())
