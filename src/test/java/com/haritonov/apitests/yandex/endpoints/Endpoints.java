5aec831088f90c0b4b6649fada7ffc8716e75d36
Nikita Haritonov <nikita.harit@mail.ru>
2026-09-08 11:07:14 +0300

    feat(enpoints): add ApiConfig and Enpoints classes

---
+++ src/test/java/com/haritonov/apitests/yandex/endpoints/Endpoints.java
@@ -0,0 +1,15 @@
+package com.haritonov.apitests.yandex.endpoints;
+
+/**
+ * Класс-хранилище констант эндпоинтов API Яндекс.Диска.
+ */
+public final class Endpoints {
+
+    private Endpoints() {
+    }
+
+    /**
+     * Эндпоинт для получения информации о Диске пользователя.
+     */
+    public static final String DISK = "/v1/disk/";
+}
